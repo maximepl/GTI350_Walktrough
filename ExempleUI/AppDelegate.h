3bc155a43b44cbe9cc45944d4aba4fb6532294c7
Pelletier-Legault, Maxime <am49650@logti-a3412-26>
2017-05-11 08:46:15 -0400

    Initial Commit

---
+++ ExempleUI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExempleUI
+//
+//  Created by Pelletier-Legault, Maxime on 2017-05-11.
+//  Copyright © 2017 Pelletier-Legault, Maxime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExempleUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExempleUI
+//
+//  Created by Pelletier-Legault, Maxime on 2017-05-11.
+//  Copyright © 2017 Pelletier-Legault, Maxime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
